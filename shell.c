f07460c2e551a69865ae4452f0c2891cecb8281f
Octavian Bodnariu <boctavian96@yahoo.com>
2016-12-06 17:11:10 +0200

    Basic terminalv2

--- shell.c
+++ shell.c
@@ -1,5 +1,69 @@
 #define LSH_RL_BUFSIZE 1024
+//pentru split_line
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
+int lsh_launch(char *args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  (if pid == 0)
+  {
+    if(execvp(args[0], args) == -1)
+    {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  }else if(pid < 0)
+        {
+          //Eroare 
+          perror("lsh");
+        }
+        else
+        {
+          //Procesul parinte
+          do
+          {
+            wpid = waitpid(pid, &status, WUNTRACED);
+          }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+  return 1;
+}
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(*char));
+  char *token;
+
+  if(!tokens)
+  {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while(token != NULL)
+  {
+    tokens[position] = token;
+    position++;
+    
+    if(position >= bufsize)
+    {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(*char));
+      if(!tokens)
+      {
+        fprintf(stderr, "lsh: allocation error");
+        exit(EXIT_FAILURE);
+      }
+    }
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
 
 char *lsh_read_line(void)
 {
