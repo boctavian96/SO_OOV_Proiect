03bd9c1001dd646c2f8b282109dbc85ddf2b9425
Anisorac Vasile <anisorac@yahoo.com>
2016-12-08 23:50:14 +0200

    Update help.c

--- help.c
+++ help.c
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
                 return 1;
         }
 
-	if (argv[1]=="my_cal")
+	if (argv[1]=="my_cal")// afisarea pe linia de comanda a documentatiei pentru comanda my_yes
 	{
 		fd = open("my_cal.txt", O_RDONLY);
 
@@ -36,12 +36,27 @@ int main(int argc, char* argv[])
 		close(fd);
 	
 	 }
- 	if (argv[1]== "-d" && argv[2]=="my_cal")
+ 	if (argv[1]== "-d" && argv[2]=="my_cal")// descrierea intr-un rand a comenzii my_call
 	{
 		printf("displays a calendar and the date of Easter\n");
 	}
+ 	if (argv[1]=="my_yes")// afisarea pe linia de comanda a documentatiei pentru comanda my_yes
+	{
+
+
+		fd = open("my_yes.txt", O_RDONLY);
+
+		 buff = malloc(sizeof(char *) * SIZE);
+                        while ((bytesRead = read(fd, buff, SIZE)) > 0)
+                        {
+                                buff[bytesRead] = '\0';
+                                printf("%s", buff);
+                        }
+                        free(buff);
+		close(fd);
 	
-	if (argv[1]== "--version" && argv[2]=="my_yes")
+	}	
+	if (argv[1]== "--version" && argv[2]=="my_yes")// afisarea versiunii comenzii my_yes
 	{
 		printf("yes (GNU coreutils) 8.25 \n Copyright (C) 2016 Free Software Foundation, Inc.\n");
 	}
