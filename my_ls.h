ea4531adb1642cd3b3da85c32e9ec9956270aea4
Anisorac Vasile <anisorac@yahoo.com>
2016-12-14 00:02:31 +0200

    Update my_ls.h

--- my_ls.h
+++ my_ls.h
@@ -5,25 +5,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+//PATH=$PATH:$HOME/proiect/
+// export PATH
+// $PATH
+
+#define SIZE 10
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-int my_ls(char** argumente)
+int main (int argc, char* argv[])
 {
-
+unsigned char isFile =0x8;
+unsigned char isFolder =0x4;
 DIR *dir;
 struct dirent *ent;
+int status;
+
+
 if ((dir = opendir (".")) != NULL) {
   /* afiseaza toate folderele si fisierele */
   while ((ent = readdir (dir)) != NULL) {
+		;
 
-    printf (ANSI_COLOR_YELLOW "%s\n" ANSI_COLOR_RESET , ent->d_name );
+	if (ent->d_type == isFile) {
+        printf (ANSI_COLOR_YELLOW "%s\n" ANSI_COLOR_RESET , ent->d_name );
+    }
+    
+	if (ent->d_type == isFolder) {
+        printf (ANSI_COLOR_BLUE "%s\n" ANSI_COLOR_RESET , ent->d_name );
+    }
+
+
+
+
   }
-  closedir(dir);
-} else {
-  /* nu se poate deschide directorul*/
+  closedir (dir);
+} 
+
+else {
+  /* nu poate deschide directorul*/
   perror ("");
   return EXIT_FAILURE;
 }
-return 1;
-}	
+}
