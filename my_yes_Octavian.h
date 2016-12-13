e494568127a4e9592be317085c6525e425d1879a
Octavian Bodnariu <boctavian96@yahoo.com>
2016-12-13 10:26:42 +0200

    No bugs....

--- my_yes_Octavian.h
+++ my_yes_Octavian.h
@@ -2,18 +2,27 @@
 #include<stdio.h>
 #include<string.h>
 /*
-Autor: Octavian Bodnariu
-Versiune: 1.1
+Autor: Octavian Bodnariu Versiune: 1.1
 */
+int size(char **argumente)
+{
+  int size = 0;
+  while(argumente[++size] != '\0');
+  return size;
+}
 
 int my_yes(char **argumente)
 {
   char *c;
   const char *ver = "Versiunea 1.0\nAutor:Octavian Bodnariu\nEmail:boctavian96@yahoo.com\n";
-  int argc = sizeof(argumente)/sizeof(*argumente);
+//  int argc = sizeof(argumente)/sizeof(argumente[0]);
+  int argc = size(argumente);
   int arglen = argc>1 ? strlen(argumente[1] + 1) : 0;
   int i = 1;
   char *argument = malloc(sizeof(char));
+
+  printf("Args: %d %s %s %s", argc, argumente[0], argumente[1], argument);
+
 
   if(argc > 1)
     while(i < argc)
@@ -22,6 +31,9 @@ int my_yes(char **argumente)
       argument = strcat(argument, " ");
       i++;
     }
+
+  printf("\n%s\n", argument);
+
 
   if(argumente[1] != NULL)
   {
@@ -52,5 +64,6 @@ int my_yes(char **argumente)
         printf("%s \n", c);
       }while(c != NULL);
   }
+
   return 1;
 }
